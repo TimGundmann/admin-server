e8ea67e955d4495a33db12536c5074a1be669591
Tim Gundmann <tim@gundmann.dk>
2020-02-08 17:24:17 +0100

    try something new

--- src/main/java/dk/gundmann/adminserver/Application.java
+++ src/main/java/dk/gundmann/adminserver/Application.java
@@ -1,9 +1,18 @@
 package dk.gundmann.adminserver;
 
+import javax.net.ssl.SSLException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+import reactor.netty.http.client.HttpClient;
 
 @SpringBootApplication
 @EnableAdminServer
@@ -11,6 +20,20 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	
+	@Bean
+    public WebClient createWebClient() throws SSLException {
+		System.out.println("create web client");
+        SslContext sslContext = SslContextBuilder
+            .forClient()
+            .trustManager(InsecureTrustManagerFactory.INSTANCE)
+            .build();
+        HttpClient httpClient = HttpClient.create()
+            .secure(t -> t.sslContext(sslContext));
+        return WebClient.builder()
+            .clientConnector(new ReactorClientHttpConnector(httpClient))
+            .build();
     }
     
 }
--- src/main/java/dk/gundmann/adminserver/config/SecurityConfig.java
+++ src/main/java/dk/gundmann/adminserver/config/SecurityConfig.java
@@ -1,24 +1,14 @@
 package dk.gundmann.adminserver.config;
 
-import de.codecentric.boot.admin.server.config.AdminServerProperties;
-import dk.gundmann.adminserver.config.condition.SpringBootAdminSecureConditional;
-import io.netty.handler.ssl.SslContext;
-import io.netty.handler.ssl.SslContextBuilder;
-import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
-import reactor.netty.http.client.HttpClient;
-
-import javax.net.ssl.SSLException;
-
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.reactive.ClientHttpConnector;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
-import org.springframework.web.reactive.function.client.WebClient;
+
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import dk.gundmann.adminserver.config.condition.SpringBootAdminSecureConditional;
 
 @Conditional(SpringBootAdminSecureConditional.class)
 @Configuration
@@ -32,19 +22,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     public SecurityConfig(AdminServerProperties adminServerProperties) {
         this.adminContextPath = adminServerProperties.getContextPath();
-    }
-
-    @Bean
-    public WebClient createWebClient() throws SSLException {
-        SslContext sslContext = SslContextBuilder
-            .forClient()
-            .trustManager(InsecureTrustManagerFactory.INSTANCE)
-            .build();
-        HttpClient httpClient = HttpClient.create()
-            .secure(t -> t.sslContext(sslContext));
-        return WebClient.builder()
-            .clientConnector(new ReactorClientHttpConnector(httpClient))
-            .build();
     }
 
     @Override
